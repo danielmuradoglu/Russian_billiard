c1ec34ae02f477a948983346efa1c8258f12c2c6
danielmuradoglu <danielmuradoglu@gmail.com>
2026-09-25 00:14:51 +0300

    Fix JVM target compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
 android {
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
     namespace = "com.bmpodium.russianbilliards"
     compileSdk = 35
     defaultConfig { applicationId = "com.bmpodium.russianbilliards"; minSdk = 24; targetSdk = 35; versionCode = 1; versionName = "1.0" }
